fe6e594a62a692a8ec8225cb035cb36ecafd0acf
document-cat <89754779+Document-cat@users.noreply.github.com>
2024-02-02 16:58:23 +0000

    Update

--- Index.js
+++ Index.js
@@ -1,12 +1,12 @@
-function dropdownjs() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function departurejs() {
+    document.getElementById("myDeparture").classList.toggle("show");
 }
 
 function filterFunction() {
     var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
+    input = document.getElementById("departureInput");
     filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
+    div = document.getElementById("myDeparture");
     a = div.getElementsByTagName("a");
     for (i = 0; i < a.length; i++) {
       txtValue = a[i].textContent || a[i].innerText;
